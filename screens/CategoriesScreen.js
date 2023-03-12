0db882415a612a90f965584a4506d224d92c68bb
Manda Nazaire NDOUFFOU <nazairendouffou@gmail.com>
2023-03-12 15:51:39 +0000

    Style des categories et meal

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,10 +6,13 @@ import { CATEGORIES } from "../data/dummy-data";
 function CategoriesScreen({navigation}){
 
     function renderCategoryItem(itemData){
-        function pressHandler() {
-            navigation.navigate("MealsOverview");
-        }
 
+        function pressHandler() {
+            navigation.navigate("MealsOverview", {
+                categoryId: itemData.item.id
+            }
+            );
+        }
         return <CategoryGridTile title={itemData.item.title} color = {itemData.item.color} onPress = {pressHandler} />;
     }
 
